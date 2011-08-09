e7f68a9b79a06069f1036c0a509855eaf3a14e9e
Paulo Barcelos <paulo@azucrina.org>
2011-08-09 22:24:19 +0200

    I think it's donne :)

---
+++ src/ofxTimestamp.cpp
@@ -0,0 +1,37 @@
+/*
+ *  ofxTimestamp.cpp
+ *  ofxTimestampExample
+ *
+ *  Created by Paulo Barcelos on 8/9/11.
+ *  Copyright 2011 paulobarcelos.com. All rights reserved.
+ *
+ */
+
+#include "ofxTimestamp.h"
+
+///////////////////////////////////////////////////////////////////////////////////
+// setup --------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////////
+void ofxTimestamp::setup(float interval, string filename){
+	this->interval = interval;
+	this->filename = filename;
+	
+	lastElapsedTime = ofGetElapsedTimef();
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// update -------------------------------------------------------------------------
+///////////////////////////////////////////////////////////////////////////////////
+void ofxTimestamp::update(){
+	if ((ofGetElapsedTimef() - lastElapsedTime) > interval) {
+		lastElapsedTime = ofGetElapsedTimef();
+		
+		FILE * f = fopen( ofToDataPath(filename+".txt", TRUE).c_str() ,"w");
+		
+		if ( f == nil )
+			return;
+		
+		fprintf (f,"%d\n", ofGetSystemTime());
+		fclose(f);
+	}
+}
